edc1efa385291ddc1ec5835c4fb65fdd45ff0de5
NarakuNight <qimingnan23@163.com>
2021-02-17 14:15:07 +0800

    Site updated: 2021-02-17 14:15:03

--- js/page.js
+++ js/page.js
@@ -1,30 +1,45 @@
-const SERVER_URL = "http://zn.sakurayun.com:3140/";
+const SERVER_URL = "https://request.pluteam.xyz/";
+var count = 0;
 
-function loadQuestions(){
-  $.ajax({type : "get",
-  url : SERVER_URL,
-  data : "qu",
-  async : true,
-  error : function(err){
-    alert("错误:\n" + err.code);
-  },
-  success : function(data){
-    alert(data);
-  }
-})
+function addQuestion(q,s1,s2,s3,s4){
+  let form = document.createElement("form");
+  form.id = "q" + count;
+  document.getElementById("q_form").appendChild(form);
+  count++;
 }
 
-function submitID(){
-  $.ajax({type : "get",
+function get(){
+  let send = {req : "get",
+    id : document.getElementById("game_id").value,
+    ip : returnCitySN["cip"]
+  };
+  console.log(JSON.stringify(send));
+  let alert_text = document.getElementById("alert_txt");
+
+  $.ajax({type : "post",
   url : SERVER_URL,
-  data : "id" + document.getElementById("game_id").value,
+  data : JSON.stringify(send),
+  dataType : "json",
   async : true,
   error : function(err){
-    alert("错误:\n" + err.code);
+    alert_text.innerHTML = ("错误:\n" + err.code);
   },
+  headers: {
+    'Content-Type': 'application/json'
+    },
   success : function(data){
-    if(data == "true"){
-      loadQuestions();
+    try {
+      if(data == "err"){
+        alert_text.innerHTML = ("向服务器请求时发生错误");
+      }else if (data == "iprep"){
+        alert_text.innerHTML = "您的IP与已注册ID中IP重复"
+      }else if (datya = "idrep"){
+        alert_text.innerHTML = "此ID已注册"
+      }else{
+        alert_text.innerHTML = data;
+      }
+    } catch (error) {
+      alert_text.innerHTML = "发生错误:" + error;
     }
   }
 })
