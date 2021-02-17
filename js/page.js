0de7e2eb74d86635c7a2edc16fee4bdf4442c956
NarakuNight <qimingnan23@163.com>
2021-02-17 20:22:10 +0800

    Site updated: 2021-02-17 20:22:05

--- js/page.js
+++ js/page.js
@@ -1,11 +1,85 @@
 const SERVER_URL = "https://request.pluteam.xyz/";
 var count = 0;
+var qCount = 0;
 
-function addQuestion(q,s1,s2,s3,s4){
-  let form = document.createElement("form");
-  form.id = "q" + count;
-  document.getElementById("q_form").appendChild(form);
+var node = document.createElement("div");
+var text = document.createElement("div");
+var submit = document.createElement("input");
+submit.type = "button";
+submit.value = "提交";
+
+node.appendChild(text.cloneNode());
+for (i=1;i<=4;i++){
+  let div = text.cloneNode();
+  let radio = document.createElement("input");
+  radio.type = "radio";
+  radio.value = i.toString();
+  radio.name = "qu" + qCount;
+  div.appendChild(radio);
+  node.appendChild(div);
+}
+
+function updName(){
+  node.childNodes.forEach(element => {
+    element.name = "qu" + qCount;
+  });
+  qCount++;
+}
+
+function createQuestion(q){
   count++;
+  let no = node.cloneNode(true);
+  console.log(no);
+  no.id = "q" + count;
+  
+  no.childNodes[0].appendChild(document.createTextNode(count + '.' + q.con))
+  let j = 1;
+  for(i = 0;i<no.childNodes.length;i++){
+    if (i != 0){
+      no.childNodes[i].appendChild(document.createTextNode(q['s' + j]));
+      j++;
+    }
+  }
+
+  updName();
+  return no;
+}
+
+function loadTest(data){
+  console.log(data);
+  document.getElementById("head11").remove();
+  let form = document.getElementById("q_form");
+
+  data.q.forEach(element => {
+    form.appendChild(createQuestion(element));
+  });
+  let div = document.createElement("div");
+  div.style = "text-align:center";
+  div.appendChild(submit);
+  form.appendChild(div);
+  
+  let att = document.createElement("div");
+  div.style = "text-align:center";
+  let h = document.createElement("h3");
+  h.innerHTML = "制作者"
+  att.appendChild(h);
+  
+  let text = "";
+  data.author.forEach(element => {
+    text += element + "  ";
+  });
+  let h1 = document.createElement("b");
+  h1.innerHTML = "出题者 - ";
+  att.appendChild(document.createTextNode(text));
+  att.appendChild(h1);
+
+  att.appendChild(document.createElement("br"));
+  let h2 = document.createElement("b");
+  h2.innerHTML = "问卷页制作 - ";
+  att.appendChild(document.createTextNode("Naraku_Night"));
+  att.appendChild(h2);
+
+  form.appendChild(att);
 }
 
 function get(){
@@ -13,34 +87,37 @@ function get(){
     id : document.getElementById("game_id").value,
     ip : returnCitySN["cip"]
   };
-  console.log(JSON.stringify(send));
   let alert_text = document.getElementById("alert_txt");
 
-  $.ajax({type : "post",
+  $.ajax({type : "get",
   url : SERVER_URL,
-  data : JSON.stringify(send),
   dataType : "json",
+  data : JSON.stringify(send),
   async : true,
   error : function(err){
     alert_text.innerHTML = ("错误:\n" + err.code);
+    console.error(err);
   },
-  headers: {
-    'Content-Type': 'application/json'
-    },
   success : function(data){
+    console.log(data.type);
     try {
-      if(data == "err"){
+      if(data.type == "err"){
         alert_text.innerHTML = ("向服务器请求时发生错误");
-      }else if (data == "iprep"){
+      }else if (data.type == "iprep"){
         alert_text.innerHTML = "您的IP与已注册ID中IP重复"
-      }else if (datya = "idrep"){
+      }else if (data.type = "test"){
+        alert_text.innerHTML = "加载题目..."
+        loadTest(data);
+      }else if (data.type == "iperr"){
+        alert_text.innerHTML = "检查IP时出现错误";
+      }else if (data.type == "idrep"){
         alert_text.innerHTML = "此ID已注册"
-      }else{
-        alert_text.innerHTML = data;
       }
     } catch (error) {
+      console.error(error);
       alert_text.innerHTML = "发生错误:" + error;
     }
   }
 })
+  alert_text.innerHTML = "正在向服务器请求中..."
 }
